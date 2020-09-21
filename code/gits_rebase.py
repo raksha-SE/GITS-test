a02eb9f05e6cdcb1ed2485e1a6b9a6561ff214d2
raksha-SE <71579320+raksha-SE@users.noreply.github.com>
2020-09-21 14:13:21 -0400

    Update gits_rebase.py

--- code/gits_rebase.py
+++ code/gits_rebase.py
@@ -2,23 +2,38 @@ import os
 import sys
 import subprocess
 import argparse
+from subprocess import Popen, PIPE
 
 def gits_rebase(args):
     print(args)
     print("Hello from GITS command line tools- Rebase")
 
-    print("Is the rebase on current branch?")
-    inp = input("[yes/no][y/n]")
-    if inp.lower() == "yes" or inp.lower() == "y"
-        rc = os.popen("git rebase master").read()
-        print(rc)
-    else:
-        inp2 =input("Enter the name of the branch you want to rebase")
-        print(inp2)
-        rc2 = os.popen("git branch | grep "+inp2).read()
-        if rc2 == inp2 :
-            rc3 = os.popen("git checkout "+inp2+" git rebase master").read()
-            print(rc3)
+    
+      
+
+    try:
+        print("Is the rebase on current branch?")
+        inp = input("[yes/no][y/n]")
+        if inp.lower() == "yes" or inp.lower() == "y":
+            process1 = subprocess.Popen(['git', 'rebase','master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process1.communicate()
+            print(stdout)
+        else:
+            inp2 =input("Enter the name of the branch you want to rebase")
+            #print(inp2)
+            rc2 = os.popen("git branch | grep "+inp2).read()
             
+            process2 = subprocess.Popen(['git', 'checkout',inp2,'git','rebase','master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process2.communicate()
+            print(stdout)
+
+
+    except Exception as e:
+        print("ERROR: gits reset command caught an exception")
+        print("ERROR: {}".format(str(e)))
+        return False
+
+    return True
+
 
 
